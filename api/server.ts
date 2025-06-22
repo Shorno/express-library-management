8e7a0f4de6be2403e77e05eab238c0ee7c2f15fc
Shorno <fb.shorno@gmail.com>
2025-06-22 19:38:45 +0600

    config as vercel

--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,6 @@
-import "../src/env"
-import {connectDB} from "../src/config/mongoose";
-import app from "../src/app";
-const PORT = process.env.PORT || 8000
+import { connectDB } from '../src/config/mongoose';
+import {app} from "../src/app";
 
+connectDB();
 
-async function start() {
-    await connectDB();
-    const port = Number(process.env.PORT) || 5000;
-    app.listen(port, () => {
-        console.log(`Server is live at http://localhost:${PORT}`);
-    });
-}
-
-start();
+export default app;
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
-import express, {Application, Request, Response} from "express"
-import booksRoutes from "./routes/booksRoutes"
-import borrowRoute from "./routes/borrowRoute";
+import express, { Application, Request, Response } from 'express';
+import booksRoutes from './routes/booksRoutes';
+import borrowRoute from './routes/borrowRoute';
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).json("hello world")
-})
+    res.status(200).json('hello world');
+});
 
+app.use('/api/books', booksRoutes);
+app.use('/api/borrow', borrowRoute);
 
-app.use("/api/books", booksRoutes)
-app.use("/api/borrow", borrowRoute)
-
-export default app;
-
+export { app };
