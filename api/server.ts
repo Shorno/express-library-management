d7a63057ef0bfc0eee9d118ec41cb03f66fd3910
Shorno <fb.shorno@gmail.com>
2025-06-22 19:43:43 +0600

    update config

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,22 @@
-import { connectDB } from '../src/config/mongoose';
-import {app} from "../src/app";
+import express, { Application, Request, Response } from 'express';
+import booksRoutes from "../src/routes/booksRoutes";
+import borrowRoute from "../src/routes/borrowRoute";
+import {connectDB} from "../src/config/mongoose";
+
+
+const app: Application = express();
+
+app.use(express.json());
 
 connectDB();
 
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json('hello world');
+});
+
+app.use('/api/books', booksRoutes);
+app.use('/api/borrow', borrowRoute);
+
 export default app;
+
+
